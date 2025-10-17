bbdf4968ad574d6bafc63aee7f9c80aa47e099da
sudhansu97ssp <patisudhansu1996@gmail.com>
2025-10-17 16:12:12 +0530

    updated copyright

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
                 </div>
             </div>
             <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
-                Copyright {new Date().getFullYear()} © GreatStack.dev All Right Reserved.
+                Copyright {new Date().getFullYear()} © GreenChart All Right Reserved.
             </p>
         </div>
     );
